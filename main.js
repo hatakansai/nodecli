2c37da85bb7784374e34316196c7f3078daf30f4
hatakansai <kankukansai@docomo.ne.jp>
2023-06-16 17:32:15 +0900

    Md to HTML 完了

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ import { program } from "commander";
 import * as fs from "node:fs/promises";
 import { marked } from "marked";
 
+program.option("--gfm", "GFMを有効にする");
 program.parse(process.argv);
 const filePath = program.args[0];
 
+const options = program.opts();
+
+const cliOptions = {
+    gfm: options.gfm ?? false,
+};
+
 fs.readFile(filePath, { encoding: "utf8" }).then(file => {
     const html = marked.parse(file, {
-        gfm: false
+        gfm: cliOptions.gfm,
     });
     console.log(html);
 }).catch(err => {
