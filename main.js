8b9a52d5693953ac9ad6e25b5d8dca0643172110
hatakansai <66507331+hatakansai@users.noreply.github.com>
2023-06-16 16:30:35 +0900

    github管理に移行

---
+++ main.js
@@ -0,0 +1,12 @@
+import { program } from "commander";
+import * as fs from "node:fs/promises";
+
+program.parse(process.argv);
+const filePath = program.args[0];
+
+fs.readFile(filePath, { encoding: "UTF-8" }).then(file => {
+    console.log(file);
+}).catch(err => {
+    console.error(err.message);
+    process.exit(1);
+});
